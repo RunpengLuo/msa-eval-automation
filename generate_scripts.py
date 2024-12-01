383a0971e602fc034fa81484395c5185f77296b0
RunpengLuo <runpeng.luo@princeton.edu>
2024-11-30 21:13:05 -0500

    add all dataset mode

--- generate_scripts.py
+++ generate_scripts.py
@@ -29,14 +29,23 @@ if __name__ == "__main__":
     sdir = get_dir(parse_quote(conf["DEFAULT"]["slurm_dir"]))
     os.makedirs(sdir, exist_ok=True)
 
-    datasets = [*map(parse_quote, conf["DEFAULT"]["datasets"].split())]
     methods = [*map(parse_quote, conf["DEFAULT"]["methods"].split())]
 
+    datasets = [*map(parse_quote, conf["DEFAULT"]["datasets"].split())]
     fasta_files = []
-    for dataset in datasets:
-        fa_file = f"{ddir}/{dataset}{dsfx}"
-        assert os.path.isfile(fa_file), f"{fa_file} does not exists"
-        fasta_files.append((dataset, fa_file))
+    if len(datasets) == 1 and datasets[0] == '*':
+        print(f"generate scripts for all datasets under {ddir}")
+        # use all datasets
+        for f in os.listdir(ddir):
+            if f.endswith(dsfx):
+                fa_file = f"{ddir}/{f}"
+                fasta_files.append((f[:-len(dsfx)], fa_file))
+    else:
+        print(f"generate scripts for selected datasets under {ddir}")
+        for dataset in datasets:
+            fa_file = f"{ddir}/{dataset}{dsfx}"
+            assert os.path.isfile(fa_file), f"{fa_file} does not exists"
+            fasta_files.append((dataset, fa_file))
 
     for method in methods:
         for (dataset, fa_file) in fasta_files:
